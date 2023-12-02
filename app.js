4eecd07b9b1c3e9ea1cb779d9b18564e845f8e70
9527 <maoguge161@gmail.com>
2023-12-02 21:32:37 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const hostname = 'localhost';
+const port = 3000;
+
+const app = express();
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
